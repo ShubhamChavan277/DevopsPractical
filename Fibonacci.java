b62fbd1fb016df557df7b4d90cc05acc42daf406
Shubham Chavan <87538535+ShubhamChavan277@users.noreply.github.com>
2021-10-29 10:17:43 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Fibonacci {
+ 
+	public static void main(String[] args) 
+	{	
+		float count = 10;
+		int a = 0;
+		int b = 1;
+    		//Scanner scan = new Scanner(System.in);
+    		//System.out.print("Enter the value of n : \n");
+    		//count = scan.nextFloat();
+		  
+	        System.out.print("Fibonacci Series of "+count+" numbers is as follows:");
+ 
+	        for (int i = 1; i <= count; ++i)
+	        {
+	            System.out.print(a+" ");
+	      
+	            int sum = a + b;
+	            a = b;
+	            b = sum;
+	        }
+	}
+}
